6d5bc5dcdcb30e490d87aa11dddc56103c3679db
L0rivera <luis.ortizt56@gmail.com>
2025-12-03 18:25:44 -0600

    Url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://taskapplication-backend-ymhq.onrender.com",
 });
 
 // Ej: API.get("/tasks")
